a9f984e66e69591763fdaaa77ee46d9f91bd6279
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-08-05 01:12:36 -0500

    Update selection.py

--- moca/selection.py
+++ moca/selection.py
@@ -188,10 +188,11 @@ def PassTrigger(df, cat, isData=False, dataName=''):
     passTriggers    = triggersForFinalState[cat][dataName] if dataName in triggersForFinalState[cat].keys() else []
     notPassTriggers = triggersNotForFinalState[cat][dataName] if dataName in triggersNotForFinalState[cat].keys() else []
     for path in passTriggers: 
-     print(path)
-     tpass |= df[path]
-    for path in notPassTriggers: tpass = (tpass)&(df[path]==0)
-     print(path)
+      print(path)
+      tpass |= df[path]
+    for path in notPassTriggers: 
+      tpass = (tpass)&(df[path]==0)
+      print(path)
   return tpass
 
 
