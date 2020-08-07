d2be660cb0a07c9cb78814afa9e73347bdbc9c0f
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-08-07 02:06:51 -0500

    Update objects.py

--- moca/objects.py
+++ moca/objects.py
@@ -44,12 +44,12 @@ def isMuonMVA(pt, eta, dxy, dz, miniIso, sip3D, mvaTTH, mediumPrompt, tightCharg
   mask = (pt>minpt)&(abs(eta)<2.5)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.4)&(sip3D<8)&(mvaTTH>0.90)&(tightCharge==2)&(jetDeepB<0.4941)#&(mediumPrompt)
   return mask
 
-def isElecMVA(pt, eta, dxy, dz, miniIso, sip3D, mvaTTH, elecMVA, lostHits, convVeto, tightCharge, sieie, hoe, 1oeM1op,jetDeepB=0, minpt=10):
+def isElecMVA(pt, eta, dxy, dz, miniIso, sip3D, mvaTTH, elecMVA, lostHits, convVeto, tightCharge, sieie, hoe, eInvMinusPInv,jetDeepB=0, minpt=10):
   maskPOGMVA = ((pt<10)&(abs(eta)<0.8)&(elecMVA>-0.13))|((pt<10)&(abs(eta)>0.8)&(abs(eta)<1.44)&(elecMVA>-0.32))|((pt<10)&(abs(eta)>1.44)&(elecMVA>-0.08))|
                ((pt>10)&(abs(eta)<0.8)&(elecMVA>-0.86))|((pt>10)&(abs(eta)>0.8)&(abs(eta)<1.44)&(elecMVA>-0.81))|((pt>10)&(abs(eta)>1.44)&(elecMVA>-0.72))
   maskSiesie = ((abs(eta)<1.44)(sieie<0.011))&((abs(eta)>1.44)(sieie<0.030))
   mask = (pt>minpt)&(abs(eta)<2.5)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.4)&(sip3D<8)&(lostHits<1)
-         &(maskPOGMVA)&(convVeto)&(maskSieie)&(hoe<0.10)&(1oeM1op>-0.04)&(mvaTTH>0.90)&(jetDeepB<0.4941)&(tightCharge==2)
+         &(maskPOGMVA)&(convVeto)&(maskSieie)&(hoe<0.10)&(eInvMinusPInv>-0.04)&(mvaTTH>0.90)&(jetDeepB<0.4941)&(tightCharge==2)
   return mask 
 
 ids = {}
