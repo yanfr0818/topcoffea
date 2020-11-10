49aefd1741a87f067b255396fdf9a4fc95c3f078
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-11-10 12:13:19 -0600

    Update objects.py

--- moca/objects.py
+++ moca/objects.py
@@ -62,7 +62,7 @@ def isElecMVA(pt, eta, dxy, dz, miniIso, sip3D, mvaTTH, elecMVA, lostHits, convV
   maskPOGMVA = ((pt<10)&(abs(eta)<0.8)&(elecMVA>-0.13))|((pt<10)&(abs(eta)>0.8)&(abs(eta)<1.44)&(elecMVA>-0.32))|((pt<10)&(abs(eta)>1.44)&(elecMVA>-0.08))|\
                ((pt>10)&(abs(eta)<0.8)&(elecMVA>-0.86))|((pt>10)&(abs(eta)>0.8)&(abs(eta)<1.44)&(elecMVA>-0.81))|((pt>10)&(abs(eta)>1.44)&(elecMVA>-0.72))
   maskSieie  = ((abs(eta)<1.479)&(sieie<0.011))|((abs(eta)>1.479)&(sieie<0.030))
-  mask = (pt>minpt)&(abs(eta)<2.5)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.40)&(sip3D<8)&(lostHits<1)&\
+  mask = (pt>minpt)&(abs(eta)<2.5)&(abs(dxy)<0.05)&(abs(dz)<0.1)&(miniIso<0.25)&(sip3D<8)&(lostHits<1)&\
          (maskPOGMVA)&(convVeto)&(maskSieie)&(hoe<0.10)&(eInvMinusPInv>-0.04)#&(mvaTTH>0.90)&(tightCharge==2)
   return mask 
  
