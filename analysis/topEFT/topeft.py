f83a3b14f2476dbfa57ee070275713bff77ad664
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-11-21 12:04:39 -0600

    Update topeft.py

--- analysis/topEFT/topeft.py
+++ analysis/topEFT/topeft.py
@@ -109,7 +109,7 @@ class AnalysisProcessor(processor.ProcessorABC):
         e['isPres']  = isPresElec(e.pt, e.eta, e.dxy, e.dz, e.miniPFRelIso_all, e.sip3d, e.lostHits, minpt=15)
         e['isTight'] = isTightElec(e.pt, e.eta, e.dxy, e.dz, e.miniPFRelIso_all, e.sip3d, e.mvaTTH, e.mvaFall17V2Iso, e.lostHits, e.convVeto, e.tightCharge,
                                 e.sieie, e.hoe, e.eInvMinusPInv, minpt=15)
-        e['isClean'] = isClean(e, mu, deltaR=0.00)#0.05
+        e['isClean'] = isClean(e, mu, drmin=0.00)#0.05
         e['isGood']  = e['isPres'] & e['isTight'] & e['isClean']
         
         leading_e = e[e.pt.argmax()]
@@ -119,7 +119,7 @@ class AnalysisProcessor(processor.ProcessorABC):
         e_pres = e[e .isPres.astype(np.bool)]
         
         tau['isPres']= isPresTau(tau.pt, tau.eta, minpt=25)
-        tau['isClean']=isClean(tau, e_pres, deltaR=0.4) & isClean(tau, mu_pres, deltaR=0.4)
+        tau['isClean']=isClean(tau, e_pres, deltaR=0.4) & isClean(tau, mu_pres, drmin=0.4)
         tau['isGood']= tau['isPres'] & tau['isClean']
         
         tau= tau[tau.isGood.astype(np.bool)]
