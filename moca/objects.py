e3ae06842db50f3b2ecef27a203a47a703ac8761
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-11-24 14:51:02 -0600

    Update objects.py

--- moca/objects.py
+++ moca/objects.py
@@ -85,9 +85,10 @@ def isCleanElec(e, mu, drmin=0.05):
   emgoodPairs = (empairs.i0.delta_r(empairs.i1) > drmin).all()
   return emgoodPairs
  
-def isPresTau(pt, eta, minpt=25.0):
-  mask = (pt>minpt)&(abs(eta)<2.4)
-  return mask
+def isPresTau(pt, eta, dxy, dz, leadTkPtOverTauPt, idAntiMu, idAntiEle, rawIso, minpt=20.0):
+  kinematics = (pt>minpt)&(abs(eta)<2.3)&(leadTkPtOverTauPt*pt>0.5)&(dxy<1000.)&(dz<0.2)
+  loose = (idAntiMu>0.5)&(idAntiEle>0.5)&(rawIso>0.5)
+  return kinematics & loose
 
 ids = {}
 ids['isTightMuonPOG'] = isTightMuonPOG
