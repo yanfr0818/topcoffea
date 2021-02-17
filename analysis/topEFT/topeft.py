eda2fdc4092b3e938aa48c47bdc3f4ee6e35ec98
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2021-02-16 18:37:19 -0600

    Update topeft.py

--- analysis/topEFT/topeft.py
+++ analysis/topEFT/topeft.py
@@ -195,7 +195,7 @@ class AnalysisProcessor(processor.ProcessorABC):
         
         Lep2 = events['event'][events['nLep'] == 2]
         nLep2 = len(Lep2)
-        print('when n =2, nLep = %i'%(nLep2))
+        print('when n = 2, nLep = %i'%(nLep2))
 
         # Cuts
         eeSSmask   = (eeSSmask[eeSSmask].counts>0)
@@ -311,6 +311,10 @@ class AnalysisProcessor(processor.ProcessorABC):
         
         print('3L events total [eee, eem, mme, mmm] = %i [%i, %i, %i, %i]'%(neee+neem+nmme+nmmm, neee, neem, nmme, nmmm))
         
+        Lep3 = events['event'][events['nLep'] == 3]
+        nLep3 = len(Lep3)
+        print('when n = 3, nLep = %i'%(nLep3))
+        
         
         ##################################################################
         ### 4 leptons
@@ -408,6 +412,10 @@ class AnalysisProcessor(processor.ProcessorABC):
         
         print('[>4e, >4m, >4e&>4m, >4e&>4mM] = [%i, %i, %i, %i]'%(ngt4e, ngt4m, ngt4e4m, ngt4e4mM))
         print('>4l = %i'%(ngt4e+ngt4m-ngt4e4m))
+        
+        Lep4 = events['event'][events['nLep'] >= 4]
+        nLep4 = len(Lep4)
+        print('when n >= 4, nLep = %i'%(nLep4))
         
         # Get Z and W invariant masses
         #goodPairs_eeee = eeee_groups[(clos_eeee)&(isOSeeee)]
